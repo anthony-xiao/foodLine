01e64060de05173c61042ecb54d93a13fccf3337
anthony-xiao <juniorxiao01@gmail.com>
2018-08-20 00:12:47 +1200

    removed getcurrentnumber line, not required by addcustomer

--- actions/restaurant/addCustomer.js
+++ actions/restaurant/addCustomer.js
@@ -1,13 +1,11 @@
 import {showError} from '../error'
 import {addRestaurantCustomerQueue} from '../../db/restaurant'
-import {getCurrentQueueNumber} from './currentQueueNum'
 import {getTotalInQueueNumber} from './totalInQueue'
 
 export function addCustomer (id, newCustomer) {
   return (dispatch) => {
     addRestaurantCustomerQueue(id, newCustomer)
       .then(() => {
-        dispatch(getCurrentQueueNumber(id))
         dispatch(getTotalInQueueNumber(id))
       })
       .catch(() => {

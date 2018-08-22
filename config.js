8ed15c9a844cc1f049549bcfe11ad0879b86279d
anthony-xiao <juniorxiao01@gmail.com>
2018-08-23 00:24:47 +1200

    create config file for oauth

---
+++ config.js
@@ -0,0 +1,12 @@
+// export const facebook = {
+//   clientID: 'INSERT-CLIENT-ID-HERE',
+//   clientSecret: 'INSERT-CLIENT-SECRET-HERE',
+//   callbackURL: 'http://localhost:3000/auth/facebook/callback',
+//   profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+// }
+
+export const google = {
+  clientID: '1043501916493-gt5rt5ovo6r2j26bksc3qnk6me37au7k.apps.googleusercontent.com',
+  clientSecret: 'YkF1TK-EwJLma9KeGdPCxCIX',
+  callbackURL: 'http://localhost:8081/auth/google/callback'
+}
